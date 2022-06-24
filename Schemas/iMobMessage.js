379675088f88176b01885e03a410e8322d7c074f
Bomi Garuba <56232544+Bomigaruba@users.noreply.github.com>
2022-06-24 11:40:17 -0400

    Update iMobMessage.js

--- Schemas/iMobMessage.js
+++ Schemas/iMobMessage.js
@@ -1,12 +1,12 @@
 const mongoose = require("mongoose");
-const Schema = mongoose.Schema;
-const iMobMessageSchema = new Schema({//this is how we can set the fields for the data types we use
-    sender: { type: Schema.Types.ObjectId, ref: 'MobMember'},
+const schema = mongoose.Schema;
+const iMobMessageSchema = new schema({//this is how we can set the fields for the data types we use
+    sender: { type: schema.Types.ObjectId, ref: 'MobMember'},
     content:{ type: String, trim: true},
-    chat:{type: Schema.Types.ObjectId, ref: 'ChatRoom'}, 
-    readBy: [{ type: Schema.Types.ObjectId, ref: 'MobMember'}]
+    chat:{type: schema.Types.ObjectId, ref: 'ChatRoom'}, 
+    readBy: [{ type: schema.Types.ObjectId, ref: 'MobMember'}]
 }, {timestamps: true});
 
 
-var Message  =  mongoose.model('Message', iMobMessageSchema);
-module.exports =  Message;
+var message  =  mongoose.model('Message', iMobMessageSchema);
+module.exports =  message;
