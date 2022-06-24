295ed85108350e85a2b0bd6f7400b673a830dd73
Bomi Garuba <56232544+Bomigaruba@users.noreply.github.com>
2022-06-24 11:36:45 -0400

    Update iMobChatRoom.js

--- Schemas/iMobChatRoom.js
+++ Schemas/iMobChatRoom.js
@@ -1,10 +1,10 @@
 const mongoose = require("mongoose");
-const Schema = mongoose.Schema;
-const iMobDM = new Schema({//this is how we can set the fields for the data types we use
+const schema = mongoose.Schema;
+const iMobDM = new schema({//this is how we can set the fields for the data types we use
     DMName:{ type: String, trim: true},
     isGroupChat: { type: Boolean, default: false },
-    iMobMembers: [{ type: Schema.Types.ObjectId, ref: 'MobMember'}],
-    latestMessage:{type: Schema.Types.ObjectId, ref: 'Message'}
+    iMobMembers: [{ type: schema.Types.ObjectId, ref: 'MobMember'}],
+    latestMessage:{type: schema.Types.ObjectId, ref: 'Message'}
 }, {timestamps: true});
-var ChatRoom  =  mongoose.model('ChatRoom', iMobDM);
-module.exports =  ChatRoom;
+var chatRoom  =  mongoose.model('ChatRoom', iMobDM);
+module.exports =  chatRoom;
